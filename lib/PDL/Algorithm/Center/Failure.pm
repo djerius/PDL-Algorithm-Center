e4cdd6ad5dc97fa9e81529604ff951953db2b4aa
Diab Jerius <djerius@cfa.harvard.edu>
2017-10-25 17:34:44 -0400

    forgot to check in PDL::Algorithm::Center::Failure module!

---
+++ lib/PDL/Algorithm/Center/Failure.pm
@@ -0,0 +1,40 @@
+package PDL::Algorithm::Center::Failure;
+
+use strict;
+use warnings;
+
+our $VERSION = '0.04';
+
+use custom::failures();
+
+use Package::Stash;
+
+use Exporter 'import';
+our @EXPORT;
+
+BEGIN {
+
+    my @failures = qw<
+      parameter
+      iteration::limit_reached
+      iteration::empty
+    >;
+
+    custom::failures->import( __PACKAGE__, @failures );
+
+    my $stash = Package::Stash->new( __PACKAGE__ );
+
+    for my $failure ( @failures ) {
+
+        ( my $name = $failure ) =~ s/::/_/g;
+
+        $name = "${name}_failure";
+
+        $stash->add_symbol( "&$name", sub () { __PACKAGE__ . "::$failure" } );
+
+        push @EXPORT, $name;
+    }
+
+}
+
+1;
